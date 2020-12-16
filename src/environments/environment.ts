d28aea3217ced37a34cc3ce55a19e707e629ca23
Aron Cornet <acornet@avans.nl>
2020-12-16 15:49:53 +0100

    Changed environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://hcai-heart-failure-backend.herokuapp.com',
   production: false
 };
 
